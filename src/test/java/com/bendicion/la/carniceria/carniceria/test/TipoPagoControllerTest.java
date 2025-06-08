e8e32910f0e21e513f3ec16c439ffd961074f2a5
DilanGutierrez <138705122+DilanGOD02@users.noreply.github.com>
2025-06-07 19:22:57 -0600

    Pruebas

--- src/test/java/com/bendicion/la/carniceria/carniceria/test/TipoPagoControllerTest.java
+++ src/test/java/com/bendicion/la/carniceria/carniceria/test/TipoPagoControllerTest.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.bendicion.la.carniceria.carniceria.test;
-
+//PRUEBAS TEST
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
